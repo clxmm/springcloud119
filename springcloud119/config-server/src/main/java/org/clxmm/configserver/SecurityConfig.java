7345b3c8971f397c2cf62c875d51ffdcdaa70ab0
clxmm <meng_yme@163.com>
2020-06-03 21:40:55 +0800

    cloud bus 1

---
+++ springcloud119/config-server/src/main/java/org/clxmm/configserver/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.clxmm.configserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author clx
+ * @date 2020-06-03 21:17
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+
+    }
+}
